f7d33b13fec40b24d88418edab2f10cf2374c024
Joshua Lehman <joshlehman.dev@gmail.com>
2023-12-20 14:38:53 -0500

    updates

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  baseUrl: "http://localhost:8000",
+  env: {
+    codeCoverage: {
+      url: "/api/__coverage__",
+    },
+  },
+  projectId: "o1yco5",
+  // ...rest of the Cypress project config
+}
